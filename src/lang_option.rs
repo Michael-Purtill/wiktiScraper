51f899293416716b551e70cc21f2ae8fd10a271b
Michael <michaelpurtill123@hotmail.com>
2021-01-09 14:47:51 -0500

    get all the languages from wiki

--- src/lang_option.rs
+++ src/lang_option.rs
@@ -9,9 +9,36 @@ pub fn lang_option() -> String {
     let doc = Html::parse_document(&res);
 
     let table_selector = Selector::parse(".wikitable").unwrap(); //there are multiple tables so need to figure out how to get the first.
-
+    let tbody_selector = Selector::parse("tbody").unwrap();
+    let tr_selector = Selector::parse("tr").unwrap();
+    let td_selector = Selector::parse("td").unwrap();
     let tables = doc.select(&table_selector).next().unwrap();
+    let tbody = tables.select(&tbody_selector).next().unwrap();
+    let trs = tbody.select(&tr_selector);
+    
+    // let mut lang_vec = Vec::new();
 
-    return tables.html();
+    for el in trs {
+        let tds: Vec<_> = el.select(&td_selector).collect();
+
+        if tds.len() < 1 {
+            continue;
+        }
+
+        let real_td = tds[1];
+
+
+        // match td {
+        //     Some(val) => td_real = val,
+        //     None => continue,
+        // };
+
+        // let mut td_real = td_real.next_sibling().unwrap();
+
+        // lang_vec.push(real_td.text().collect::<Vec<_>>().join(""));
+        println!("{}", real_td.text().collect::<Vec<_>>().join(""));
+    }
+
+    return "cool".to_string();
 
 }
