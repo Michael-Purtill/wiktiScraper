1a5d0bb3e2287bc06298c0e63334cf1d2f76adc7
Michael <michaelpurtill123@hotmail.com>
2021-01-09 11:37:39 -0500

    select the first table (happy accident)

--- src/lang_option.rs
+++ src/lang_option.rs
@@ -10,10 +10,8 @@ pub fn lang_option() -> String {
 
     let table_selector = Selector::parse(".wikitable").unwrap(); //there are multiple tables so need to figure out how to get the first.
 
-    let huh = doc.select(&table_selector).next().unwrap();
+    let tables = doc.select(&table_selector).next().unwrap();
 
-    huh;
-
-    return String::from("test");
+    return tables.html();
 
 }
